1df8ce26f92fda3206d3aaabbae3e29ff74b0a9b
youjiaming <youjiaming@meituan.com>
2020-09-22 17:40:32 +0800

    u

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ["plugin:vue/essential", "@vue/airbnb", "@vue/typescript/recommended"],
   parserOptions: {
     ecmaVersion: 2020,
+    project:'./tsconfig.json'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
 };
---
+++ src/components/tmp.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+/* eslint-disable class-methods-use-this */
+export default class Cmp {
+  get a() {
+    const list: Array<string> = [];
+    return list;
+  }
+
+  async mounted() {
+    this.a as Array<string>;
+  }
+}
+</script>
